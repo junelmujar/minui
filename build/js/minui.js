462c7b23011e3fe2278d8811babfd6b7d2e74936
Junel Mujar <junelmujar@gmail.com>
2018-06-19 17:03:58 +1000

    Initial implementation of dropdowns for nav components

--- build/js/minui.js
+++ build/js/minui.js
@@ -481,7 +481,9 @@ const Navs = (() => {
 	const Data = {
 		TOGGLE: 'nav',
 		TARGET: 'href',
-		SELECTOR: '.nav, nav'
+		SELECTOR: '.nav, nav',
+		DROPDOWN: 'dropdown',
+		LINK: '.nav-item-link'
 	};
 
 	class Navs {
@@ -489,6 +491,8 @@ const Navs = (() => {
 		constructor() {
 			// Initialize all found tabs
 			this._navs = document.querySelectorAll('[data-toggle=' + Data.TOGGLE + ']');
+			this._dropdowns = document.querySelectorAll('[data-toggle=' + Data.DROPDOWN + ']');
+			this._links = document.querySelectorAll(Data.LINK);
 			if (typeof this._navs !== 'undefined' && this._navs) {
 				this.setup();
 				this.bindListeners();
@@ -502,13 +506,70 @@ const Navs = (() => {
 					event.preventDefault();
 					var targetNavID = this.getAttribute(Data.TARGET);
 					var targetNav = document.querySelector(targetNavID);
-					event.target.classList.toggle('nav-item__toggled');
+
+					event.target.classList.toggle('nav-item__active');
 					targetNav.classList.toggle('nav-items__open');
+				});
+			});
+			this._links.forEach(function (el, i) {
+				el.addEventListener('click', function (event) {
+					event.preventDefault();
+
+					var navItems = document.querySelectorAll('.nav-item');
+					navItems.forEach(function (el, i) {
+						if (typeof el !== 'undefined') {
+							el.classList.remove('nav-item__active');
+							if (typeof el.children !== 'undefined') {
+								for (var child of el.children) {
+									if (typeof child !== 'undefined') {
+										if (child.classList.contains('dropdown')) {
+											child.classList.remove('dropdown__visible');
+										}
+									}
+								}
+							}
+						}
+					});
+
+					var targetDropdownID = this.getAttribute('href');
+					event.target.parentNode.classList.toggle('nav-item__active');
+					if (el.nextElementSibling) {
+						el.nextElementSibling.classList.toggle('dropdown__visible');
+					}
 				});
 			});
 		}
 
+		hideDropdowns() {
+			var navItems = document.querySelectorAll('.nav-item');
+			navItems.forEach(function (el, i) {
+				if (typeof el !== 'undefined') {
+					el.classList.remove('nav-item__active');
+					if (typeof el.children !== 'undefined') {
+						for (var child of el.children) {
+							if (typeof child !== 'undefined') {
+								if (child.classList.contains('dropdown')) {
+									child.classList.remove('dropdown__visible');
+								}
+							}
+						}
+					}
+				}
+			});
+		}
+
 		bindListeners() {
+
+			var that = this;
+			document.addEventListener('keyup', function (event) {
+				if (event.keyCode == 27) {
+					that.hideDropdowns();
+				}
+			});
+
+			document.addEventListener('click', function (event) {
+				that.hideDropdowns();
+			});
 
 			var togglers = document.querySelectorAll('[data-toggle=' + Data.TOGGLE + ']');
 			if (window.outerWidth <= 768) {
