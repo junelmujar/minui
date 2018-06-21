860bb91856cd300b8b8f91c2cb17def4eccd26a7
Junel Mujar <junelmujar@gmail.com>
2018-06-21 11:56:06 +1000

    Refactored tab component; add tab doc
    - add code to show panels if there a tab item has an active state on
    page load
    - implemented a key:value pair for targeting a tab panel; much more
    direct approach
    - remove unused CSS code

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -7,17 +7,78 @@ const Tabs = (() => {
 
 	class Tabs {	
 
+		// Initialize all found tabs
 	    constructor() {
-			// Initialize all found tabs
 			this._tabs = document.querySelectorAll('[data-toggle='+Data.TOGGLE+']');
+			this._activeTabs = document.querySelectorAll('.tab__nav__item--active');
 			if (typeof this._tabs !== 'undefined' && this._tabs) {
 				this.setup();
+				this.bindListeners();
 			}
 		}
 
+		// Show panels if there are tab item(s) marked as active
 		setup() {
 			var that = this;
+			this._activeTabs.forEach(function(el, i) {
+				if (typeof el.children !== 'undefined' && el.children) {
+					for (var child of el.children) {
+						var target  = that.getTarget(child);
+						that.showPanel(child, target);
+					}
+				}
+			});
+		}	
 
+		// Get target panel
+		getTarget(tab) {
+			var target;
+			var targetParts;
+			if (typeof tab !== 'undefined' && tab) {
+				target = tab.getAttribute(Data.TARGET);
+				targetParts = target.split(":");
+			}
+			return targetParts;
+		}
+
+		showPanel(tabLink, target) {
+			// We got valid parts
+			if (target && target.length == 2) {
+				var targetString = '[data-panel-id='+target[0]+'] [data-id='+target[1]+']';
+
+				tabLink.parentNode.classList.add('tab__nav__item--active');
+
+				var targetTabPanel = document.querySelector(targetString);
+
+				// Show found tab panel
+				if (typeof targetTabPanel !== 'undefined' && targetTabPanel) {
+					for (var child of targetTabPanel.parentNode.children) {
+						var siblingType = child.tagName.toLowerCase();
+						if (child !== targetTabPanel) {
+							child.classList.remove('tab__content__panel--active');
+						}						
+					}
+						
+					targetTabPanel.classList.add('tab__content__panel--active');
+				}
+			} else {
+				throw new TypeError('Tab target requires the following format and should be unique: <parent>:<id> Example: tab-dogs:german or recipes:pasta');
+			}
+		}
+
+		// Remove any active state on sibling elements
+		clearActive(tab) {
+			if (typeof tab.parentNode.parentNode.children !== 'undefined' 
+				&& tab.parentNode.parentNode.children) {
+				for (var child of tab.parentNode.parentNode.children) {
+			    	child.classList.remove('tab__nav__item--active');
+				}
+			}
+		}
+
+		// Bind tab item click event listener
+		bindListeners() {
+			var that = this;
 			this._tabs.forEach(function(el, i) {
 
 				// Show our tab
@@ -25,31 +86,21 @@ const Tabs = (() => {
 
 					event.preventDefault();
 
-					that._tabs.forEach((tabItem, index) => {
-				    	tabItem.parentNode.classList.remove('tab-item__active');
-					});
+					// Clicked tab item
+					var tab = event.target;
 
-					var targetTabPanelID = event.target.getAttribute(Data.TARGET);
+					// Clear previous or currently selected tab
+					that.clearActive(tab);
 
-					event.target.parentNode.classList.add('tab-item__active');
+					// Get target panel
+					var target = that.getTarget(event.target);
 
-					var targetTabPanel = document.querySelector(targetTabPanelID);
-
-					// Show found tab panel
-					if (typeof targetTabPanel !== 'undefined' && targetTabPanel) {
-						for (var child of targetTabPanel.parentNode.children) {
-							var siblingType = child.tagName.toLowerCase();
-							if (child !== targetTabPanel) {
-								child.classList.remove('tab-panel__active');
-							}						
-						}
-							
-						targetTabPanel.classList.add('tab-panel__active');
-					}
+					// Make current tab item active and show tab panel
+					that.showPanel(tab, target);
 
 				});
 			});		
-		}	
+		}
 
 	}
 	return Tabs;
